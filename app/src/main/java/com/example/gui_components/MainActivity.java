6e9a4e6ac496d6d5de19a02c019fdba60fa02403
yuvaraj-csk <134052574+yuvaraj-csk@users.noreply.github.com>
2024-03-11 09:23:45 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/gui_components/MainActivity.java
+++ app/src/main/java/com/example/gui_components/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.gui_components;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    Float textSize,add=2.0f;
+    Button colorButton;
+    Button fontButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        textView = findViewById(R.id.textView);
+        colorButton = findViewById(R.id.button);
+        fontButton = findViewById(R.id.button2);
+
+        colorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int randomColor = Color.rgb(
+                        (int) (Math.random() * 256),
+                        (int) (Math.random() * 256),
+                        (int) (Math.random() * 256)
+                );
+                textView.setTextColor(randomColor);
+            }
+        });
+        fontButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textSize=textView.getTextSize();
+                textSize=textSize+add;
+                textView.setTextSize(textSize);
+            }
+        });
+
+}
 }
